622d3f791c48745823e548d673b192483f84bfba
freya heaton <freyaheaton94@gmail.com>
2023-02-05 17:31:24 +0100

    push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,4 +19,52 @@ class PagesController < ApplicationController
 
   def emily
   end
+
+  def mara
+  end
+
+  def jefi
+  end
+
+  def sunita
+  end
+
+  def samalegra
+  end
+
+  def samantha
+  end
+
+  def willie
+  end
+
+  def britt
+  end
+
+  def sam
+  end
+
+  def lique
+  end
+
+  def duneisle
+  end
+
+  def alina
+  end
+
+  def marga
+  end
+
+  def meeslateesha
+  end
+
+  def isis
+  end
+
+  def maudy
+  end
+
+  def robinlisa
+  end
 end
